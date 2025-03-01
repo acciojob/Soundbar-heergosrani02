b3698aea358bc4c72ac0b31784e04796736e6c0d
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-03-01 17:55:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let btns = document.querySelectorAll(".btn");
 let stop = document.querySelector(".stop");
-const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+const audio = document.getElementById("audio");
 
 btns.forEach(btn => {
     btn.addEventListener("click", () => {
