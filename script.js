f11dfd4478a55000302cc31f87031223c60f2e8d
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-03-01 17:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 //your JS code here. If required.
 let btns = document.querySelectorAll(".btn");
 let stop = document.querySelector(".stop");
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
 
 btns.forEach(btn => {
     btn.addEventListener("click", () => {
-        let soundId = btn.getAttribute("data-sound");
-        let audio = document.getElementById(soundId);
         audio.play();
     });
 });
 
 stop.addEventListener("click", () => {
-    let audios = document.querySelectorAll("audio");
+   let audios = document.querySelectorAll("audio");
     audios.forEach(audio => {
         audio.pause();
     });
