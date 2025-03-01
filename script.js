e596d246d628e87809a49c3cda6e2622faafdf62
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-03-01 17:29:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let btns = document.querySelectorAll(".btn");
+let stop = document.querySelector(".stop");
+
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        let soundId = btn.getAttribute("data-sound");
+        let audio = document.getElementById(soundId);
+        audio.play();
+    });
+});
+
+stop.addEventListener("click", () => {
+    let audios = document.querySelectorAll("audio");
+    audios.forEach(audio => {
+        audio.pause();
+    });
+});
